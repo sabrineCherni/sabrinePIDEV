16a727649a20ba04a33452e275e3f9773b34cab8
AyatAymen <47719053+AyatAymen@users.noreply.github.com>
2019-11-29 02:27:41 +0100

    .net Ayat

---
+++ ASP-NET/MyTimesheet.Web/Controllers/EmployeController.cs
@@ -0,0 +1,125 @@
+﻿using MyTimesheet.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTimesheet.Web.Controllers
+{
+    public class EmployeController : Controller
+    {
+        // GET: Employe
+        public ActionResult Index()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080/timesheet-pidev-web/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("api/users").Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<Employe>>().Result;
+            }
+            else
+            {
+                ViewBag.result = "error";
+            }
+            return View();
+        }
+
+        // GET: Employe/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employe/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: Employe/Create
+        [HttpPost]
+        public ActionResult Create(Employe evm)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:9080/timesheet-pidev-web/");
+
+            client.PostAsJsonAsync<Employe>("api/users", evm).ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Employe/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080/timesheet-pidev-web/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("api/users/"+id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                ViewBag.result = response.Content.ReadAsAsync<Employe>().Result;
+            }
+            else
+            {
+                ViewBag.result = "error";
+            }
+            
+            return View(ViewBag.result);
+        }
+
+        // POST: Employe/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id,Employe evm)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:9080/timesheet-pidev-web/");
+
+            client.PutAsJsonAsync<Employe>("api/users", evm).ContinueWith((putTask) => putTask.Result.EnsureSuccessStatusCode());
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Employe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080/timesheet-pidev-web/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("api/users/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                ViewBag.result = response.Content.ReadAsAsync<Employe>().Result;
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            
+
+            return View(ViewBag.result);
+        }
+
+        // POST: Employe/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Employe emp)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:9080/timesheet-pidev-web/");
+
+            client.DeleteAsync("api/users/"+id).ContinueWith((deleteTask) => deleteTask.Result.EnsureSuccessStatusCode());
+
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ASP-NET/MyTimesheet.Web/Controllers/ProjetController.cs
@@ -0,0 +1,104 @@
+﻿using MyTimesheet.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTimesheet.Web.Controllers
+{
+    public class ProjetController : Controller
+    {
+
+        ProjetService ps;
+        public ProjetController()
+        {
+            ps = new ProjetService();
+        }
+        // GET: Projet
+        public ActionResult Index()
+        {
+            return View(ps.GetAll());
+        }
+        [HttpPost]
+        public ActionResult Index(string filtre)
+        {
+            var list = ps.GetMany();
+
+            if (!String.IsNullOrEmpty(filtre))
+            { list = list.Where(m => m.title_projet.ToString().Equals(filtre)).ToList(); }
+            return View(list);
+        }
+        // GET: Projet/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Projet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Projet/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Projet/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Projet/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Projet/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Projet/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
